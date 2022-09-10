ed980b8c3d138b33f4129ba322bbb5b195e32f86
James Clarke <j.kingston.c@gmail.com>
2022-09-10 11:10:01 +0100

    got actions working

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,73 @@
+pub trait CommandPart {
+    fn ingest(&self, cmd: &std::string::String);
+}
+
+pub struct PositionalCommandPart{
+    cmd: std::string::String,
+    action: Box<dyn Fn()>
+}
+
+impl core::fmt::Debug for PositionalCommandPart {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("PositionalCommandPart")
+         .field("cmd", &self.cmd)
+         .field("action", &"<action>")
+         .finish()
+    }
+}
+
+impl CommandPart for PositionalCommandPart {
+    fn ingest(&self, cmd: &std::string::String){
+        if cmd.eq(&self.cmd){
+            (self.action)();
+        }
+    }
+}
+
 pub struct Command{
-    pub cmd: std::string::String,
-    pub action: std::string::String
+    pub parts: std::vec::Vec<Box<dyn CommandPart>>
+}
+
+impl Command {
+    pub fn exec(&self, cmd: &std::string::String) -> Result<(), ()>{
+        for (_i, e) in self.parts.iter().enumerate() {
+            e.ingest(&cmd)
+        }
+        return Ok(());
+    }
 }
 
 pub struct Commandeer{
-    pub cmds: std::vec::Vec<Command>
+    // todo ideally we want a tree of command parts?
+    pub cmds: std::vec::Vec<Command>,
+    pub unknown: std::string::String
 }
 
 impl Commandeer{
     
-    pub fn cmd(&mut self, cmd: std::string::String, action: std::string::String){
-        println!("registering command `{}`.", cmd);
-        self.cmds.push(Command{cmd: cmd, action: action});
+    pub fn cmd(&mut self, cmd: std::string::String, action: Box<dyn Fn()>){
+        self.cmds.push(Command{
+            parts: vec!(
+                Box::new(PositionalCommandPart{cmd: cmd, action: action})
+            )
+        });
+    }
+
+    pub fn unknown(&mut self, unknown: std::string::String){
+        self.unknown = unknown;
     }
 
     pub fn exec(&mut self, cmd: std::string::String){
-        println!("executing `{}`.", cmd);
-
-        for val in self.cmds.iter(){
-            if val.cmd.eq(&cmd){
-                println!("{}", val.action);
+        let mut foundMatch = false;
+        for command in self.cmds.iter(){
+            match command.exec(&cmd) {
+                Ok(_) => foundMatch = true,
+                Err(_) => todo!()
             }
+
+        }
+        if !foundMatch {
+            println!("{}", self.unknown);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@
 pub fn main(){
 
 
-    let mut commandeer = commandeer::Commandeer{cmds: vec!()};
+    let mut commandeer = commandeer::Commandeer{cmds: vec!(), unknown: std::string::String::from("")};
     commandeer.cmd(
         std::string::String::from("help"),
-        std::string::String::from("press h for help"));
+        Box::new(||println!("press help for help!",)));
+
+    commandeer.unknown(std::string::String::from("unknown command, press help for help"));
+
     commandeer.exec(std::string::String::from("help"));
 
 
